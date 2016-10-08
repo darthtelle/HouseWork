292384b611ff3d423df94c05c89050606fab8a32
Chantelle Porritt <chantelleporritt@gmail.com>
2016-10-08 22:43:22 +0100

    Player Movement/Grid System Setup

---
+++ HouseWork/Assets/Scripts/GridSystem.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridSystem : MonoBehaviour
+{
+	[SerializeField]
+	private IntVector2 m_SquareSize;
+
+	[SerializeField]
+	private IntVector2 m_GridSize;
+
+	private Grid[] m_GridList;
+
+	private void Awake()
+	{
+		m_GridList = new Grid[m_GridSize.x * m_GridSize.y];
+
+		for(int xIndex = 0; xIndex < m_GridSize.x; xIndex++)
+		{
+			int ID = xIndex;
+
+			Vector3 position = new Vector3(m_SquareSize.x * xIndex, 0.0f, 0.0f);
+
+			m_GridList[ID] = new Grid() { m_ID = ID, m_Position = position };
+		}
+	}
+
+	private void OnDrawGizmos()
+	{
+		for(int yIndex = 0; yIndex < m_GridSize.y; yIndex++)
+		{
+			for(int xIndex = 0; xIndex < m_GridSize.x; xIndex++)
+			{
+				Vector3 position = new Vector3(m_SquareSize.x * xIndex, 0.0f, m_SquareSize.y * yIndex);
+				Gizmos.DrawWireCube(position, new Vector3(m_SquareSize.x, 0.0f, m_SquareSize.y));
+			}
+		}
+	}
+
+	private struct Grid
+	{
+		public int m_ID;
+		public Vector3 m_Position;
+	}
+}
---
+++ HouseWork/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	private CharacterController m_CharacterController;
+	private Vector3 m_MoveDirection;
+
+	private const float k_Gravity = 20.0f;
+	public float k_MaxSpeed = 5.0f;
+	public float k_JumpSpeed = 8.0f;
+
+	private void Awake()
+	{
+		m_CharacterController = gameObject.GetComponent<CharacterController>();
+		m_MoveDirection = Vector3.zero;
+
+		/*if(m_BodyObject == null)
+		{
+			Debug.LogError("ERROR: Body object is null.");
+		}*/
+	}
+
+	private void Update()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		m_MoveDirection.x = horizontal * k_MaxSpeed;
+		m_MoveDirection.z = vertical * k_MaxSpeed;
+
+		m_CharacterController.Move(m_MoveDirection * Time.deltaTime);
+
+		Flip();
+	}
+
+	private void Flip()
+	{
+		/*if(((m_MoveDirection.x > 0.0f) && (m_BodyObject.transform.localScale.x < 0.0f)) || ((m_MoveDirection.x < 0.0f) && (m_BodyObject.transform.localScale.x > 0.0f)))
+		{
+			Vector3 scale = m_BodyObject.transform.localScale;
+			scale.x *= -1.0f;
+			m_BodyObject.transform.localScale = scale;
+		}*/
+	}
+}
