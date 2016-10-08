9478c5218e39758a17ab686690a3b26bc4166de4
Chantelle Porritt <chantelleporritt@gmail.com>
2016-10-08 17:40:01 +0100

    Unity Setup

---
+++ HouseWork/Assets/Editor/SpriteProcessor_Tool.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace ChantelleTools
+{
+	public class SpriteProcessor_Tool : AssetPostprocessor 
+	{
+		private void OnPostprocessTexture(Texture2D texture)
+		{
+			string lowerCaseAssetPath = assetPath.ToLower();
+			bool isInSpritesDirectory = lowerCaseAssetPath.IndexOf("/sprites/") != -1;
+
+			if(isInSpritesDirectory)
+			{
+				TextureImporter textureImporter = (TextureImporter)assetImporter;
+				textureImporter.textureType = TextureImporterType.Sprite;
+			}
+		}
+	}
+}
---
+++ HouseWork/Assets/Editor/TagSelection_Tool.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace ChantelleTools
+{
+	public class TagSelection_Tool : ScriptableWizard
+	{
+		public string m_SearchTag = "Tag name";
+
+		[MenuItem("AmazingChantelleTools/Tag Selection")]
+		private static void TagSelectionMenu()
+		{
+			ScriptableWizard.DisplayWizard<TagSelection_Tool>("Tag Selection", "Search");
+		}
+
+		private void OnWizardCreate()
+		{
+			GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(m_SearchTag);
+			Selection.objects = gameObjects;
+		}
+	}
+}
